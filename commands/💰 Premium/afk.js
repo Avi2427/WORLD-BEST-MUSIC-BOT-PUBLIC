fe91c3cc00ed7c59379b983d40965f16e7909166
ItsAvi_ <79397844+Avi2427@users.noreply.github.com>
2021-10-06 11:46:36 +0530

    Updated

--- commands/💰 Premium/afk.js
+++ commands/💰 Premium/afk.js
@@ -46,28 +46,7 @@ module.exports = {
           .setDescription(`Channelname: \`${message.guild.channels.cache.get(player.voiceChannel).name}\``)
         );
 
-      //get the database information for the GUILD
-      let gpremium = client.premium.get(message.guild.id);
-      //get the database information for the USER
-      let ppremium = client.premium.get(message.author.id);
-      //these few lines create the Stringarray of each OWNER USER of the config.json
-      let ownerstringarray = ``;
-      for (let i = 0; i < config.ownerIDS.length; i++) {
-        try {
-          let user = await client.users.fetch(config.ownerIDS[i]);
-          ownerstringarray += `\`${user.tag}\` /`
-        } catch {}
-      }
-      //save the string
-      ownerstringarray = ownerstringarray.substr(0, ownerstringarray.length - 2);
-      //if no enabled premiums, return
-      if (!gpremium.enabled && !ppremium.enabled)
-        return message.channel.send(new MessageEmbed()
-          .setColor(ee.wrongcolor)
-          .setFooter(ee.footertext, ee.footericon)
-          .setTitle(`${emoji.msg.ERROR}  Error | No Premium Commands Available`)
-          .setDescription(`Dm to get premium:\n ${ownerstringarray}`.substr(0, 2040))
-        )
+      
       //if no args --> return with information
       if (!args[0]) {
         return message.channel.send(new MessageEmbed()
@@ -80,15 +59,7 @@ module.exports = {
         )
       }
       //if args 0 is guild
-      if (args[0].toLowerCase() === `guild`) {
-        //if guild premium is not enabled return error
-        if (!gpremium.enabled)
-          return message.channel.send(new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter(ee.footertext, ee.footericon)
-            .setTitle(`${emoji.msg.ERROR}  Error | No Premium Commands Available for this Guild`)
-            .setDescription(`Dm to get premium:\n ${ownerstringarray}`.substr(0, 2040))
-          );
+      
         //toggle the database state
         player.set(`afk-${message.guild.id}`, !player.get(`afk-${message.guild.id}`))
         //return information message
@@ -99,16 +70,7 @@ module.exports = {
           .setDescription(`For the Guild: \`${message.guild.name}\``)
         );
       }
-      //if the args 0 is user
-      else if (args[0].toLowerCase() === `user`) {
-        //if user premium is not enbaled return error
-        if (!ppremium.enabled)
-          return message.channel.send(new MessageEmbed()
-            .setColor(ee.wrongcolor)
-            .setFooter(ee.footertext, ee.footericon)
-            .setTitle(`${emoji.msg.ERROR}  Error | No Premium Commands Available for you`)
-            .setDescription(`Dm to get premium:\n ${ownerstringarray}`.substr(0, 2040))
-          );
+      
         //the creater of the player, did not have premium! return error
         if (message.author.id !== player.get(`playerauthor`))
           return message.channel.send(new MessageEmbed()
